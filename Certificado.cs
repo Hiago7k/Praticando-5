8f96aa09c098b830950e0b4df53eb0777a622372
Hiago 7k <hiago7k@gmail.com>
2026-03-04 20:34:05 -0300

    novo desafio itemdigital

--- Certificado.cs
+++ Certificado.cs
@@ -1,17 +1,1 @@
-﻿namespace Praticando_5;
-
-internal class Certificado 
-{
-    public Profissao Profissao;
-    public Certificado(Profissao profissao)
-    {
-        Profissao = profissao;
-    }
-    public void AdicionarCertificado()
-    {
-        Console.WriteLine($"Certificado emitido para : {Profissao.Titulo}");
-    }
-    
-
-
-}
+﻿
---
+++ ItemDigital.cs
@@ -0,0 +1,11 @@
+﻿namespace Praticando_5;
+
+internal class ItemDigital
+{
+    public ItemDigital(string tituloDoItem)
+    {
+        TituloDoItem = tituloDoItem;
+    }
+
+    public string TituloDoItem { get;  }
+}
