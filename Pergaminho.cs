10adec6669f43d9c6a293e8b9ab03d0ee2f6a3f7
Hiago 7k <hiago7k@gmail.com>
2026-03-04 20:39:08 -0300

    finalizando desafio

---
+++ Pergaminho.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Praticando_5;
+
+internal class Pergaminho : ItemDigital
+{
+    public string Armazenar { get; set; }
+    public Pergaminho(string titulodoitem, string armazenar)
+        :base(titulodoitem)
+    {
+        Armazenar = armazenar;
+    }
+
+
+    public void MostrarDetalhes()
+    {
+        Console.WriteLine($"Detalhes do Pergaminho");
+        Console.WriteLine($"Titulo: {TituloDoItem}");
+        Console.WriteLine($"Descricao {Armazenar}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿using Praticando_5;
 
-Analista analista = new Analista("Analista de Sistemas");
-Docente docente = new Docente("Docente de Matematica");
-Certificado cert1 = new Certificado(analista);
-Certificado cert2 = new Certificado(docente);
-
-cert1.AdicionarCertificado();
-cert2.AdicionarCertificado();
+Pergaminho pergaminhoAntigo = new Pergaminho("Segredos_Antigos.txt", "A chave para a sabedoria reside na observação...");
+pergaminhoAntigo.MostrarDetalhes();
